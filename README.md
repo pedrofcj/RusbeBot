a958c2b42252d6e55c42ee8696a7ce7a3ddf1e16
Pedro Costa <pedrofcj@gmail.com>
2022-04-14 23:22:10 +0100

    Renomeada pastas e arquivos

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# TheLostBot
-Bot criado para auxiliar (e zoar) no Discord do The Lost do servidor de RP Kamikaze
+# Rusbé Bot
+Bot inicialmente criado para auxiliar (e zoar) no Discord do The Lost do servidor de RP Kamikaze
 
 ## Config
 O arquivo de configuração tem as chaves da AWS (caso queira usar Dynamo DB) e o token do seu bot.
 
-Caso queira rodar esse bot no seu servidor, altere o arquivo em `TheLostBot/_config.yml`.
+Caso queira rodar esse bot no seu servidor, altere o arquivo em `RusbeBot/_config.yml`.
--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
-dotnet publish -r linux-arm .\TheLostBot\TheLostBot.csproj
-Remove-Item .\TheLostBot\bin\Debug\net6.0\linux-arm\publish\_config.yml
-ssh pi@raspberrypi "pgrep TheLost | xargs kill"
-scp .\TheLostBot\bin\Debug\net6.0\linux-arm\publish\* pi@raspberrypi:/home/pi/TheLostBot
-ssh pi@raspberrypi ./TheLostBot/TheLostBot
+dotnet publish -r linux-arm .\RusbeBot\RusbeBot.csproj
+Remove-Item .\RusbeBot\bin\Debug\net6.0\linux-arm\publish\_config.yml
+ssh pi@raspberrypi "pgrep RusbeBot | xargs kill"
+scp .\RusbeBot\bin\Debug\net6.0\linux-arm\publish\* pi@raspberrypi:/home/pi/RusbeBot
+ssh pi@raspberrypi ./RusbeBot/RusbeBot
